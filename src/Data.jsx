75592ade3ada96850a8a5e56c28bd93430965461
Andre Silva <hardsys_andre@hotmail.com>
2024-10-18 23:21:47 -0300

    fix-ajuste do link das imagens

--- src/Data.jsx
+++ src/Data.jsx
@@ -3,7 +3,7 @@ export const destaques = [
     id: 1,
     nome: 'Vestido',
     preco: 160.00,
-    imagem: 'src/assets/vestido.jpeg',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/vestido.jpeg',
     tipo: 'vestido',
     descricao:
       'Vestido longo verde com estampa de bolinhas brancas, perfeito para dias ensolarados. Com um decote coração e alças finas, ele traz elegância e frescor ao look. O tecido leve proporciona conforto e movimento, ideal para passeios ao ar livre ou eventos casuais. Combine com sandálias ou tênis para um visual descontraído.',
@@ -12,7 +12,7 @@ export const destaques = [
     id: 2,
     nome: 'Macaquinho',
     preco: 98.00,
-    imagem: 'src/assets/macaquinho.png',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/macaquinho.png',
     tipo: 'macaquinho',
     descricao:
       'Macacão curto estampado floral, com alças delicadas e cintura marcada, ideal para dias quentes e ocasiões casuais. O fundo claro realça a vibrante estampa de flores e folhagens, proporcionando um look leve e descontraído. Perfeito para passeios ou encontros, pode ser combinado com sandálias ou rasteirinhas para um visual fresco e elegante.',
@@ -21,7 +21,7 @@ export const destaques = [
     id: 3,
     nome: 'Pijama',
     preco: 79.00,
-    imagem: 'src/assets/pijama.png',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/pijama.png',
     tipo: 'pijama',
     descricao:
       'Camisolas combinando para mãe e filha, em um delicado tom rosa claro com estampa de coroa e frases amorosas. Confeccionadas em tecido leve e confortável, essas peças são perfeitas para uma noite de sono tranquila ou para momentos especiais de conexão entre mãe e filha. Um conjunto fofo e carinhoso que celebra o vínculo entre as duas.',
@@ -30,7 +30,7 @@ export const destaques = [
     id: 4,
     nome: 'Vestido Laranja',
     preco: 190.00,
-    imagem: 'src/assets/plus-size-laranja.png',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/plus-size-laranja.png',
     tipo: 'plus-size',
     descricao:
       'Este vestido plus size em um vibrante tom de laranja é a escolha perfeita para quem busca um look marcante e cheio de estilo. Com um design moderno e um caimento confortável, ele valoriza as curvas sem abrir mão da leveza. O tecido fluido e o corte ajustado na cintura criam uma silhueta harmoniosa, ideal para eventos casuais ou um passeio descontraído. Combine com sandálias ou acessórios neutros para um visual elegante e alegre.',
@@ -39,7 +39,7 @@ export const destaques = [
     id: 5,
     nome: 'Vestido Colorido',
     preco: 190.00,
-    imagem: 'src/assets/plus-size-colorido.png',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/plus-size-colorido.png',
     tipo: 'plus-size',
     descricao:
       'O vestido plus size florido traz toda a feminilidade e frescor das estampas florais. Com fundo claro e flores coloridas, essa peça é perfeita para dias de primavera e verão. O corte soltinho garante conforto e liberdade de movimento, enquanto o detalhe acinturado oferece um toque delicado à silhueta. Seja para um almoço ao ar livre ou uma ocasião especial, este vestido combina praticidade e beleza, destacando-se por seu charme natural.',
@@ -51,7 +51,7 @@ export const roupas = [
     id: 1,
     nome: 'Vestido',
     preco: 160.00,
-    imagem: 'src/assets/vestido.jpeg',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/vestido.jpeg',
     tipo: 'vestido',
     descricao:
       'Vestido longo verde com estampa de bolinhas brancas, perfeito para dias ensolarados. Com um decote coração e alças finas, ele traz elegância e frescor ao look. O tecido leve proporciona conforto e movimento, ideal para passeios ao ar livre ou eventos casuais. Combine com sandálias ou tênis para um visual descontraído.',
@@ -60,7 +60,7 @@ export const roupas = [
     id: 2,
     nome: 'Macaquinho',
     preco: 98.00,
-    imagem: 'src/assets/macaquinho.png',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/macaquinho.png',
     tipo: 'macaquinho',
     descricao:
       'Macacão curto estampado floral, com alças delicadas e cintura marcada, ideal para dias quentes e ocasiões casuais. O fundo claro realça a vibrante estampa de flores e folhagens, proporcionando um look leve e descontraído. Perfeito para passeios ou encontros, pode ser combinado com sandálias ou rasteirinhas para um visual fresco e elegante.',
@@ -69,7 +69,7 @@ export const roupas = [
     id: 3,
     nome: 'Pijama',
     preco: 79.00,
-    imagem: 'src/assets/pijama.png',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/pijama.png',
     tipo: 'pijama',
     descricao:
       'Camisolas combinando para mãe e filha, em um delicado tom rosa claro com estampa de coroa e frases amorosas. Confeccionadas em tecido leve e confortável, essas peças são perfeitas para uma noite de sono tranquila ou para momentos especiais de conexão entre mãe e filha. Um conjunto fofo e carinhoso que celebra o vínculo entre as duas.',
@@ -78,7 +78,7 @@ export const roupas = [
     id: 4,
     nome: 'Vestido Laranja',
     preco: 190.00,
-    imagem: 'src/assets/plus-size-laranja.png',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/plus-size-laranja.png',
     tipo: 'plus-size',
     descricao:
       'Este vestido plus size em um vibrante tom de laranja é a escolha perfeita para quem busca um look marcante e cheio de estilo. Com um design moderno e um caimento confortável, ele valoriza as curvas sem abrir mão da leveza. O tecido fluido e o corte ajustado na cintura criam uma silhueta harmoniosa, ideal para eventos casuais ou um passeio descontraído. Combine com sandálias ou acessórios neutros para um visual elegante e alegre.',
@@ -87,7 +87,7 @@ export const roupas = [
     id: 5,
     nome: 'Vestido Colorido',
     preco: 190.00,
-    imagem: 'src/assets/plus-size-colorido.png',
+    imagem: 'https://raw.githubusercontent.com/Hardsys-Andre/Loja-de-Roupas-Online/master/src/assets/plus-size-colorido.png',
     tipo: 'plus-size',
     descricao:
       'O vestido plus size florido traz toda a feminilidade e frescor das estampas florais. Com fundo claro e flores coloridas, essa peça é perfeita para dias de primavera e verão. O corte soltinho garante conforto e liberdade de movimento, enquanto o detalhe acinturado oferece um toque delicado à silhueta. Seja para um almoço ao ar livre ou uma ocasião especial, este vestido combina praticidade e beleza, destacando-se por seu charme natural.',
